34cdc62ac7af642e7db836dadf7d0fb8c2a8c04d
dennis.bohn <dennis.bohn@bohnmedia.de>
2023-12-14 02:04:58 +0100

    Ordnerstruktur optimiert

---
+++ src/EventListener/Page/GeneratePageListener.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BohnMedia\ContaoMultifileuploadBundle\EventListener\Page;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\PageRegular;
+use Contao\LayoutModel;
+use Contao\PageModel;
+
+#[AsHook('generatePage')]
+class GeneratePageListener
+{
+    public function __invoke(PageModel $pageModel, LayoutModel $layout, PageRegular $pageRegular): void
+    {
+        $GLOBALS['TL_CSS'][] = 'bundles/contaomultifileupload/css/multifileupload.css';
+    }
+}
